7a470e92b9102a57b7685636d2158c8172fd6f88
DaloLorn <DaloLorn@users.noreply.github.com>
2025-08-17 12:12:07 +0200

    Fixed a syntax error and tidied up a bit more.

--- src/types.ts
+++ src/types.ts
@@ -2,25 +2,25 @@ import { window } from 'vscode';
 
 export const PREFIX = 'opensr-file-cloning';
 
-export enum CONFIGS {
-	BASE_GAME_FOLDER = `${PREFIX}.baseGameFolder`,
-	REGISTERED_MODS = `${PREFIX}.registeredMods`,
-}
+export const CONFIGS = {
+	BASE_GAME_FOLDER: `${PREFIX}.baseGameFolder`,
+	REGISTERED_MODS: `${PREFIX}.registeredMods`,
+};
 
-export enum COMMANDS {
-	FIND_BASE_FILES = `${PREFIX}.findBaseFiles`,
-	REGISTER_MOD = `${PREFIX}.registerMod`,
-	REREGISTER_MOD = `${PREFIX}.reregisterMod`,
-	CLONE_BASE_FILE = `${PREFIX}.cloneBaseFile`,
-	CLONE_MOD_FILE = `${PREFIX}.cloneModFile`,
-	CLONE_ANCESTOR_FILE = `${PREFIX}.cloneAncestorFile`,
-	COMPARE_BASE_FILE = `${PREFIX}.compareBaseFile`,
-	COMPARE_MOD_FILE = `${PREFIX}.compareModFile`,
-	COMPARE_ANCESTOR_FILE = `${PREFIX}.compareAncestorFile`,
-	COMPARE_TO_BASE = `${PREFIX}.compareToBase`,
-	COMPARE_TO_MOD = `${PREFIX}.compareToMod`,
-	COMPARE_TO_ANCESTOR = `${PREFIX}.compareToAncestor`,
-}
+export const COMMANDS = {
+	FIND_BASE_FILES: `${PREFIX}.findBaseFiles`,
+	REGISTER_MOD: `${PREFIX}.registerMod`,
+	REREGISTER_MOD: `${PREFIX}.reregisterMod`,
+	CLONE_BASE_FILE: `${PREFIX}.cloneBaseFile`,
+	CLONE_MOD_FILE: `${PREFIX}.cloneModFile`,
+	CLONE_ANCESTOR_FILE: `${PREFIX}.cloneAncestorFile`,
+	COMPARE_BASE_FILE: `${PREFIX}.compareBaseFile`,
+	COMPARE_MOD_FILE: `${PREFIX}.compareModFile`,
+	COMPARE_ANCESTOR_FILE: `${PREFIX}.compareAncestorFile`,
+	COMPARE_TO_BASE: `${PREFIX}.compareToBase`,
+	COMPARE_TO_MOD: `${PREFIX}.compareToMod`,
+	COMPARE_TO_ANCESTOR: `${PREFIX}.compareToAncestor`,
+};
 
 export const LOGGER = window.createOutputChannel('OpenSR Toolset', {
 	log: true,
@@ -32,7 +32,9 @@ export interface ModFile {
 	modName: string;
 	path: string;
 }
+
 export type ModInfo = Omit<ModFile, 'path'>;
+
 export function resolvePath(file: ModFile) {
 	return `${file.modPath}/${file.path || MODINFO}`;
 }
@@ -40,10 +42,12 @@ export function resolvePath(file: ModFile) {
 export type StringOptional = string | undefined;
 export type ModinfoField = 'Derives From' | 'Name';
 export type FileFindable = StringOptional | ModInfo;
+
 export type FileOperation = (
 	source: ModFile,
 	destination: ModFile,
 ) => Promise<void>;
+
 export type FileFinder<T extends FileFindable> = (
 	purpose: string,
 	modinfo: T,
